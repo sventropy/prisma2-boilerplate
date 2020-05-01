8e5348c137a45a4ca2dc8e4e9d9fc467eabecdb9
sventropy <sven@hennessen.net>
2020-05-01 13:50:01 +0200

    custom query resolvers, one using prisma API, one using RAW sql

--- src/schema/Query.ts
+++ src/schema/Query.ts
@@ -1,9 +1,36 @@
 import { queryType } from "nexus";
+import { FieldResolver } from "nexus/dist/core";
+
+const headerCountResolver: FieldResolver<"Query", "headerCount"> = async (
+  _root,
+  _args,
+  ctx,
+) => {
+  return ctx.prisma.header.count();
+};
+
+const itemCountResolver: FieldResolver<"Query", "itemCount"> = async (
+  _root,
+  _args,
+  ctx,
+) => {
+  const result: [{ count: number }] = await ctx.prisma
+    .raw`SELECT COUNT(*) FROM "Item";`;
+  return result.length ? result[0].count : 0;
+};
 
 export const Query = queryType({
   definition(t) {
     t.crud.header();
     t.crud.headers();
     t.crud.items();
+    t.field("headerCount", {
+      type: "Int",
+      resolve: headerCountResolver,
+    });
+    t.field("itemCount", {
+      type: "Int",
+      resolve: itemCountResolver,
+    });
   },
 });
