83d085af3bafb6733c2d2c9ea902ce443bc181e1
sventropy <sven@hennessen.net>
2020-06-01 13:05:57 +0200

    renamed react client and changed client layout

--- clients/react-client/src/App.tsx
+++ clients/react-client/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { Query } from "react-apollo";
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./GraphQlClient";
-import { Table, Container } from "react-bootstrap";
+import { Table, Container, Jumbotron } from "react-bootstrap";
 import gql from "graphql-tag";
 import { HeaderItemQuery } from "./graphql/HeaderItemQuery";
 
@@ -22,32 +22,35 @@ const HEADER_ITEM_QUERY = gql`
 
 function App() {
   const createTables = (data: HeaderItemQuery) => (
-    <>
-      {data.headers.map(h => (
-        <Table key={h.title}>
-          <thead>
-            <tr>
-              <td>Title</td>
-              <td>Type</td>
-              <td>Created At</td>
+    <Table>
+      <thead>
+        <tr>
+          <td>Header</td>
+          <td>Item</td>
+          <td>Type</td>
+          <td>Created At</td>
+        </tr>
+      </thead>
+      <tbody>
+        {data.headers.map(h =>
+          h.items.map(i => (
+            <tr key={h.title + "-" + i.title}>
+              <td>{h.title}</td>
+              <td>{i.title}</td>
+              <td>{i.type}</td>
+              <td>{i.createdAt}</td>
             </tr>
-          </thead>
-          <tbody>
-            {h.items.map(i => (
-              <tr key={h.title + "-" + i.title}>
-                <td>{i.title}</td>
-                <td>{i.type}</td>
-                <td>{i.createdAt}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      ))}
-    </>
+          )),
+        )}
+      </tbody>
+    </Table>
   );
   return (
     <ApolloProvider client={client}>
       <Container>
+        <Jumbotron>
+          <h1>React GraphQL Client</h1>
+        </Jumbotron>
         <Query query={HEADER_ITEM_QUERY}>
           {({ loading, data }: { loading: boolean; data: HeaderItemQuery }) => {
             return loading ? <>Loading...</> : createTables(data);
